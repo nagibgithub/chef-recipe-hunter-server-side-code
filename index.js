77d9d924bbf16a9673f6f2e796659eeb9e0eeb22
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-05-04 16:38:56 +0600

    all recipe dynamic link

--- index.js
+++ index.js
@@ -21,6 +21,25 @@ app.get('/chefs/:chefId', (req, res) => {
     res.send(chef);
 });
 
+app.get('/recipes', (req, res) => {
+    let x = [];
+    for (const chefAff of chefs) {
+        x = [...x, ...chefAff.recipes]
+    }
+    res.send(x)
+});
+
+app.get('/recipes/:recipeID', (req, res) => {
+    const id = req.params.recipeID;
+    let x = [];
+    for (const chefAff of chefs) {
+        x = [...x, ...chefAff.recipes]
+    }
+    const recipe = x.find(pd => pd.rec_id == id);
+    res.send(recipe)
+})
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${ port }`);
 });
